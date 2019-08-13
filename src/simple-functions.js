d96749062d78c87ff92b5c6b60f0bcc65127e218
olegskvortsov <o.skvortsov.developer@gmail.com>
2019-08-13 09:13:01 +0300

    Add Readme

--- src/simple-functions.js
+++ src/simple-functions.js
@@ -1,5 +1,7 @@
+// сумма через замыкание summRec(a)(b)()
 const summRec = a => b => (!!b && summRec(a + b)) || a;
 
+// сумма через замыкание summCloser(a)(b)
 function summCloser(a) {
   let current = a;
 
@@ -14,18 +16,21 @@ function summCloser(a) {
   return f;
 }
 
+// Проверка на простоту
 function isPrime(number) {
   let del = 2;
   while (del * del <= number && number % del !== 0) del += 1;
   return number > 2 && del * del > number;
 }
 
+// Факториал
 function factorial(number) {
   let result = 1;
   for (let i = 1; i <= number; i++) result *= i;
   return result;
 }
 
+// Фибоначи
 function fib(number) {
   let a = 1;
   let b = 1;
@@ -39,6 +44,8 @@ function fib(number) {
   return number !== 0 ? b : 0;
 }
 
+
+// Проверка на сортировку
 function isSorted(arr) {
   if (arr.length < 2) return true;
   const direct = arr[0] < arr[1] ? 'up' : 'down';
@@ -48,12 +55,15 @@ function isSorted(arr) {
   });
 }
 
+// свой фильтер
 function filter(arr, predicate) {
   const result = [];
   arr.forEach(item => predicate && predicate(item) && result.push(item));
   return result;
 }
 
+
+// совй редьюс
 function reduce(arr, fn, acc = null) {
   arr.forEach((item, index) => {
     acc = fn(acc, item, index);
